74c0d61ecea446d192f106ca83372e06296c7afe
joaquindicio <joacodicio@gmail.com>
2024-03-08 14:26:08 -0300

    adding netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: "https://joaquindicio.github.io",
   base: "/manglar-de-bagheera/",
+  output: "server",
+  adapter: netlify()
 });
